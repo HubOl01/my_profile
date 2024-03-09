c57a6439788acdd009a71895ca50e00c4205e481
hubol01 <kuloleg52@gmail.com>
2024-03-09 15:50:08 +0300

    added skills

---
+++ .history/src/components/center/Common_20240309095045.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import styles from './styles.module.scss'
+import { Grid } from '@mui/material'
+import { CircularProgressWithLabel } from '../atoms/circularProgressWithLabel'
+import { my_skills } from '../../data/my_skills'
+
+export default function Common() {
+  return (
+    <div className={styles.center_common}>
+      <h2>Немного о себе</h2>
+      <p>
+
+      </p>
+      <h2>
+        Мои навыки
+      </h2>
+      <Grid container margin={2} rowGap={5}>
+        {
+          my_skills.map((skill) =>
+            <Grid xs={6} sx={{
+
+              'textAlign': "center",
+              // verticalAlign: "middle",
+              // lineHeight: "normal"
+            }}>
+              <div style={{ "background": "red" }}>
+                <div
+                  style={
+                    {
+                      // top: 0,
+                      // left: 0,
+                      // bottom: 0,
+                      // right: 0,
+                      // position: 'absolute',
+                      display: 'inline-flex',
+                      alignItems: 'center',
+                      // justifyContent: 'center',
+                      // verticalAlign: "middle",
+                      // height: "100%",
+                      // 'textAlign': "center",
+
+                      // lineHeight: "normal"
+                    }
+                  }>
+                  {skill.name}
+                </div>
+                <CircularProgressWithLabel value={skill.progress * 100} />
+
+              </div>
+            </Grid>
+          )
+        }
+        {/*         
+        <Grid xs={6} ><CircularProgressWithLabel value={my_skills[0].progress}/></Grid> */}
+        {/* <Grid xs={6} ><CircularProgressWithLabel value={}/><</Grid>
+        <Grid xs={6} ><CircularProgressWithLabel value={}/><</Grid>
+        <Grid xs={6} ><CircularProgressWithLabel value={}/><</Grid>
+        <Grid xs={6} ><CircularProgressWithLabel value={}/><</Grid>
+        <Grid xs={6} ><CircularProgressWithLabel value={}/><</Grid> */}
+      </Grid>
+      {/* <h2>Хобби</h2>
+      <p>
+        Я люблю изучать что-то новое. Я программирую на языке программирования
+        C# и продолжаю осваивать язык в некоторых направлениях. (Например:
+        создание мобильных приложений Xamarin и создание игр Unity)
+      </p>
+      <h2>Спорт</h2>
+      <p>
+        Я более 10 лет занимаюсь конькобежным спортом. В 16 лет выполнил
+        спортивный разряд кандидат в мастера спорта. Кроме этого, с начала 1
+        курса я начал заниматься легкой атлетикой. Принимаю участие в
+        соревнованиях в составе сборной ИГХТУ.
+      </p> */}
+    </div>
+  )
+}
---
+++ .history/src/components/center/Common_20240309154841.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import styles from './styles.module.scss'
+import { Box, Grid } from '@mui/material'
+import { my_skills } from '../../data/my_skills'
+import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar'
+
+export default function Common() {
+  return (
+    <div className={styles.center_common}>
+      <h2>Немного о себе</h2>
+      <p>
+
+      </p>
+      <h2>
+        Мои навыки
+      </h2>
+      <Grid container margin={2} rowGap={0}>
+        {
+          my_skills.map((skill) =>
+            <Grid xs={6}
+            >
+              <Box sx={{
+                // height: "50px",
+                "textAlign": "right",
+                // background: "red",
+              }}
+              >
+                <Grid container padding={2}>
+                  <Grid xs={8} sx={{
+                    minWidth: "100px",
+                    textAlign: "left",
+                    margin: "auto"
+                  }}>{skill.name}</Grid>
+                  <Grid xs={4} sx={{ textAlign: "right", margin: "auto", minWidth: "80px" }}>
+                    {/* <CircularProgressWithLabel value={skill.progress * 100} /> */}
+                    <Box sx={{
+                      maxWidth: "100px"
+                    }}>
+
+                    <CircularProgressbarWithChildren
+                      value={skill.progress * 100}
+                      strokeWidth={10}
+                      // text={`${skill.progress * 100}%`}
+                      styles={buildStyles({
+                        // textColor: "white",
+                        pathColor: "white",
+                        // textSize: "20px"
+                        // trailColor: "gold"
+                      })}
+                    >
+                      <div style={{ fontSize: "medium ", marginTop: -15}}>
+                        {`${skill.progress * 100}%`}
+                      </div>
+                    </CircularProgressbarWithChildren>
+                    </Box>
+                  </Grid>
+                </Grid>
+
+
+
+
+
+
+
+              </Box>
+            </Grid>
+          )
+        }
+       
+      </Grid>
+      <h2>
+        Проекты
+      </h2>
+      
+    </div>
+  )
+}
